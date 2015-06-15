dff145273385ef6b9580cd1e10becba272212e99
greyjustice <gkwright@wrightweb.co.uk>
2015-06-15 20:39:45 +0100

    Initial Commit

---
+++ Heart Rate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heart Rate
+//
+//  Created by Graham Wright on 15/06/2015.
+//  Copyright (c) 2015 Graham Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
