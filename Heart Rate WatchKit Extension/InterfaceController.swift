ed7197a65b73b727b58ae9e687be492685e386c7
greyjustice <gkwright@wrightweb.co.uk>
2015-11-28 14:06:14 +0000

    add to git hub

---
+++ Heart Rate WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Heart Rate WatchKit Extension
+//
+//  Created by Graham Wright on 15/06/2015.
+//  Copyright (c) 2015 Graham Wright. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
